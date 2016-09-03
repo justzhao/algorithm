72fb5248515e84a11d7e3a010a48a423fa817462
zhaopeng <zp6282154@163.com>
2016-09-03 14:43:41 +0800

    misc: 螺旋数组新增递归解法

--- src/main/java/com/zhaopeng/question/SpiralArray.java
+++ src/main/java/com/zhaopeng/question/SpiralArray.java
@@ -10,7 +10,7 @@ public class SpiralArray {
     public  static  void  main(String args[]){
 
         /**
-         * 思路 ：设数组长 高都为n 则从  n*n 开始 最小为1 一共 n*n个数字
+         * 思路 1 常规解法 ：设数组长 高都为n 则从  n*n 开始 最小为1 一共 n*n个数字
          * 数组在初始化过程中，有四种转弯情况，设 row 为行，col 为列
          * 从 0行 0列开始 array[0][0]
          * 1  右边 向下转  col 不变  row++
@@ -18,12 +18,29 @@ public class SpiralArray {
          * 3 左向上转  col 不变  row--
          * 4 上向右转  row 不变 col++
          *
+         * 思路2： 递归解法，螺旋数组，先初始化外层，再初始化内层，一层一层的初始化。
+         *
+         *  设定 长高都为n。row 为行，col为列。开始row=0  col=n-1.
+         *  1 先初始化最上面一列，
+         *  2 初始化右边的一排
+         *  3 初始化最下面的一列
+         *  4 初始化最左边的一排
+         *   递归下一层：
+         *    当  row>col  递归结束
+         *
+         *     1   2    3   4
+         *    12  13   14   5
+         *    11  16   15   6
+         *    10  9    8    7
          */
         System.out.println("123");
 
         int len=10;
-        int [][]arry=initArray(len);
 
+        int [][]arry=new int[len][len];
+        //arry= initArray(len);// 常规
+
+        initArrayRecursive(arry,0,len,len*len);// 递归
        print(arry,len);
 
     }
@@ -38,6 +55,30 @@ public class SpiralArray {
             System.out.println();
         }
 
+
+    }
+
+    public  static  void initArrayRecursive(int [][]array,int rows,int col,int value){
+
+        if(rows>col)return;
+        //最上面开始
+        for(int i=rows ;i<col;i++){
+            array[rows][i]=value--;
+        }
+        //最右边开始 ,注意连接点，应该是第二行开始
+        for(int i=rows+1;i<col;i++){
+            array[i][col-1]=value--;
+        }
+        //最下面开始 ，注意连接点， 应该是从倒数第二列开始开始
+        for(int i=col-2;i>rows;i--){
+            array[col-1][i]=value--;
+        }
+        //最左边开始，注意连接点，从倒数第一排开始
+        for (int i=col-1;i>rows;i--){
+            array[i][rows]=value--;
+        }
+
+        initArrayRecursive(array,rows+1,col-1,value);
 
     }
 
@@ -77,6 +118,7 @@ public class SpiralArray {
         return  nextStep;
     }
 
+    // 常规解法
     public static int[][] initArray(int len){
 
         int [][]array=new int[len][len];
