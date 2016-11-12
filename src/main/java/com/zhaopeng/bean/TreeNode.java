ee632cfa00cbaf0ddd5a59290ab5143f38ea6ea4
zhaopeng <zp6282154@163.com>
2016-11-13 00:43:34 +0800

    msic:加入treenode 定义

---
+++ src/main/java/com/zhaopeng/bean/TreeNode.java
@@ -0,0 +1,15 @@
+package com.zhaopeng.bean;
+
+/**
+ * Created by zhaopeng on 2016/11/12.
+ */
+public class TreeNode {
+
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+
+   public TreeNode(int x) {
+        val = x;
+    }
+}
