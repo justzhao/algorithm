76be9ba3a2374cab842c5a8fdc23479ccecaace6
zhaopeng <zp6282154@163.com>
2016-11-14 00:33:08 +0800

    msic:二叉树路径打印第二版本 去栈

---
+++ src/main/java/com/zhaopeng/question/BinaryTreePathsV2.java
@@ -0,0 +1,47 @@
+package com.zhaopeng.question;
+
+import com.zhaopeng.bean.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+/**
+ * 获取二叉树根到叶子节点的所有路径 第二个版本
+ * Created by zhaopeng on 2016/11/13.
+ * <p>
+ *    1
+ * /   \
+ * 2     3
+ * \
+ * 5
+ */
+
+public class BinaryTreePathsV2 {
+
+    List<String> list = new ArrayList<String>();
+    public List<String> binaryTreePaths(TreeNode root) {
+        String result="";
+        traversTreePath(root,result);
+        return list;
+    }
+
+    public void traversTreePath(TreeNode node, String result) {
+         //空树判断
+        if(node==null){
+            return;
+        }
+        result+=node.val;
+        if(node.left==null&&node.right==null){
+            list.add(result);
+            return;
+        }
+        result+="->";
+        if(node.left!=null){
+            traversTreePath(node.left,result);
+        }
+        if(node.right!=null){
+            traversTreePath(node.right,result);
+        }
+    }
+}
