f70341c03ef4ea690145331fce03f814e4dbbc15
zhaopeng <zp6282154@163.com>
2016-11-20 23:48:27 +0800

    msic:二叉搜索树

---
+++ src/main/java/com/zhaopeng/question/SortedArrayToBinarySearchTree.java
@@ -0,0 +1,31 @@
+package com.zhaopeng.question;
+
+import com.zhaopeng.bean.TreeNode;
+
+/**
+ * Created by zhaopeng on 2016/11/20.
+ */
+public class SortedArrayToBinarySearchTree {
+    public TreeNode sortedArrayToBST(int[] nums) {
+
+        if(nums==null||nums.length==0){
+            return null;
+        }
+
+        return bulidBST(0,nums.length-1,nums);
+
+    }
+    public TreeNode bulidBST(int start,int end,int []nums){
+
+        if(start>end){
+            return null;
+        }
+        int mid=(start+end)/2;
+        TreeNode root=new TreeNode(nums[mid]);
+        root.left=bulidBST(start,mid-1,nums);
+        root.right=bulidBST(mid+1,end,nums);
+
+        return root;
+    }
+
+}
